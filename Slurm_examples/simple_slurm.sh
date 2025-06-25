95b6fdb5829d8ebd4000eff58fd8806d7d635c58
Peter <peter.r.rock2@gmail.com>
2025-06-25 13:17:47 -0600

    Made some very simple scripts

---
+++ Slurm_examples/simple_slurm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --mem=1G  # Request 1 GB of memory
+#SBATCH --time=00:05:00  # Request 5 minutes of runtime
+#SBATCH --job-name=example_job  # Set the job name to "example_job"
+#SBATCH --output=simple_slurm_output.out  # Redirect output to a file
+#SBATCH --error=simple_slurm_error.log  # Redirect error messages to a file
+
+
+# Load any necessary modules or environment variables here
+echo "Hello from the Slurm job script!" # This will appear in the output file
+echo "This is an error appearing in the slurm job script!" >&2 # This will appear in the error file
+
